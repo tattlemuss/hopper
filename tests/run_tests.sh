baba5985f240dbe77bda9cb156584843b9863127
Steven Tattersall <2294514+tattlemuss@users.noreply.github.com>
2023-03-09 11:55:56 +0000

    Rename test1.s -> test_68000.s

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,19 +1,21 @@
 #!/usr/bin/env sh
 
-rm test1.prg
-rm test_68020.bin
+rm *.prg
+rm *.bin
+rm *.diff
+rm *.txt
 
-vasmm68k_mot test1.s -Ftos -devpac -o test1.prg
+vasmm68k_mot test_68000.s -Ftos -devpac -o test_68000.prg
 vasmm68k_mot test_68020.s -m68020 -Ftos -o test_68020.bin
 
 # Disassemble and compare
 
 echo "test 1"
-../src/bin/hopper test1.prg > test1.txt
+../src/bin/hopper test_68000.prg > test_68000.txt
 echo "test 2"
 ../src/bin/hopper --m68020  test_68020.bin > test_68020.txt
 echo "diffs"
-diff --ignore-all-space test1.s test1.txt > test1.diff
+diff --ignore-all-space test_68000.s test_68000.txt > test_68000.diff
 diff --ignore-all-space test_68020.s test_68020.txt > test_68020.diff
 
 
