ef7c69fc6b7ecba9fe804f07b18e0dcd5c6432b0
Steven Tattersall <2294514+tattlemuss@users.noreply.github.com>
2023-03-26 15:58:28 +0100

    Rename test_* files to tst*
    (This is simply so Hatari can distinguish between the .prg files when
    loading from startup, for testing hrdb.)

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -5,23 +5,23 @@ rm *.bin
 rm *.diff
 rm *.txt
 
-vasmm68k_mot test_68000.s -Ftos -devpac -o test_68000.prg
-vasmm68k_mot test_68020.s -m68020 -Ftos -o test_68020.prg
-vasmm68k_mot test_68030.s -m68030 -Ftos -o test_68030.prg
+vasmm68k_mot tst68000.s -Ftos -devpac -o tst68000.prg
+vasmm68k_mot tst68020.s -m68020 -Ftos -o tst68020.prg
+vasmm68k_mot tst68030.s -m68030 -Ftos -o tst68030.prg
 
 # Disassemble and compare
 
 echo "test 68000"
-../src/bin/hopper test_68000.prg > test_68000.txt
+../src/bin/hopper tst68000.prg > tst68000.txt
 echo "test 68020"
-../src/bin/hopper --m68020  test_68020.prg > test_68020.txt
+../src/bin/hopper --m68020  tst68020.prg > tst68020.txt
 echo "test 68030"
-../src/bin/hopper --m68030  test_68030.prg > test_68030.txt
+../src/bin/hopper --m68030  tst68030.prg > tst68030.txt
 echo "diffs"
 DIFF_OPTS="--ignore-blank-lines --ignore-all-space"
 
-diff $DIFF_OPTS test_68000.s test_68000.txt > test_68000.diff
-diff $DIFF_OPTS test_68030.s test_68030.txt > test_68030.diff
-diff $DIFF_OPTS test_68020.s test_68020.txt > test_68020.diff
+diff $DIFF_OPTS tst68000.s tst68000.txt > tst68000.diff
+diff $DIFF_OPTS tst68030.s tst68030.txt > tst68030.diff
+diff $DIFF_OPTS tst68020.s tst68020.txt > tst68020.diff
 
 
