a3bf3bcc4c92115ed0dadf09eabcd0dfca9af85d
Steven Tattersall <2294514+tattlemuss@users.noreply.github.com>
2023-03-29 18:35:23 +0100

    Extract common printing functions out to print.cpp
    It should be possible to copy and edit these files to produce custom output.

---
+++ src/print.h
@@ -0,0 +1,21 @@
+// Sample functions to write an instruction to a file pointer.
+// It should be easy to update this to write to other text streams, or
+// representations that suit the use-case.
+#ifndef PRINT_H
+#define PRINT_H
+
+#include <stdint.h>
+#include <stdio.h>
+
+// Forward declarations
+struct instruction;
+struct operand;
+class symbols;
+
+// Check if an opcode jumps to another known address, and return that address
+extern bool calc_relative_address(const operand& op, uint32_t inst_address, uint32_t& target_address);
+
+// Write out an instruction's opcode and operands to the file stream.
+extern void print(const instruction& inst, const symbols& symbols, uint32_t inst_address, FILE* pFile);
+
+#endif
