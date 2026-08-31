69fc9365c6169ae1f07e2c921d6294ed5072c4e8
Steven Tattersall <2294514+tattlemuss@users.noreply.github.com>
2026-08-31 12:36:05 +0100

    ... and another printf

--- hopper68/main.cpp
+++ hopper68/main.cpp
@@ -238,7 +238,6 @@ void add_reference_symbols(const disassembly& disasm, symbols& symbols)
 		{
 			target_address = line.inst.op0.absolute_long.longaddr;
 			symbol sym;
-			printf("Target %x\n", target_address);
 			if (target_address >= first_address &&
 				target_address < last_address &&
 				!find_symbol(symbols, target_address, sym))
